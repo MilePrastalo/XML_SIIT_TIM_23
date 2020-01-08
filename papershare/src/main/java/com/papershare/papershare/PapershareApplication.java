a7c573d5d6f9210e60896d3be289fd6df50373ee
Nikola Saric <nikolasaric531@gmail.com>
2020-01-08 18:41:05 +0100

    Created intial Spring project.

---
+++ papershare/src/main/java/com/papershare/papershare/PapershareApplication.java
@@ -0,0 +1,13 @@
+package com.papershare.papershare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapershareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapershareApplication.class, args);
+	}
+
+}
