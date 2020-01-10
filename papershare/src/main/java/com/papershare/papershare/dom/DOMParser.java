95222b3708b05802e3c69b80a1113c9598b62ab4
MilePrastalo <mile.prastalo97@gmail.com>
2020-01-10 18:32:27 +0100

    added dom parser

---
+++ papershare/src/main/java/com/papershare/papershare/dom/DOMParser.java
@@ -0,0 +1,52 @@
+package com.papershare.papershare.dom;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import static org.apache.xerces.jaxp.JAXPConstants.*;
+
+@Component
+public class DOMParser {
+
+	private DocumentBuilderFactory factory;
+
+	private Document document;
+
+	/*
+	 * Factory initialization static-block
+	 */
+	public DOMParser() {
+		factory = DocumentBuilderFactory.newInstance();
+
+		/* Uključuje validaciju. */
+		factory.setValidating(false);
+
+		factory.setNamespaceAware(true);
+		factory.setIgnoringComments(true);
+		factory.setIgnoringElementContentWhitespace(true);
+
+		/* Validacija u odnosu na XML šemu. */
+		factory.setAttribute(JAXP_SCHEMA_LANGUAGE, W3C_XML_SCHEMA);
+	}
+
+	public Document buildDocument(String filePath) throws SAXException, IOException, ParserConfigurationException {
+
+		DocumentBuilder builder = factory.newDocumentBuilder();
+
+		document = builder.parse(new File(filePath));
+
+		/* Detektuju eventualne greske */
+		if (document != null)
+			System.out.println("[INFO] File parsed with no errors.");
+		else
+			System.out.println("[WARN] Document is null.");
+		return document;
+
+	}
+
+}
