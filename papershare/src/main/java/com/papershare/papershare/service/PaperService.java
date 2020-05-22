a9e53b0468dbbcdc3a7970578ac7a5b76ce1c137
jelenacuk <jelena.cuk.1997@gmail.com>
2020-05-22 11:59:05 +0200

    Small changes

--- papershare/src/main/java/com/papershare/papershare/service/PaperService.java
+++ papershare/src/main/java/com/papershare/papershare/service/PaperService.java
@@ -1,5 +1,26 @@
 package com.papershare.papershare.service;
 
-public class PaperService {
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
 
+import com.papershare.papershare.dom.XSLTransformer;
+import com.papershare.papershare.repository.PaperRepository;
+
+@Service
+public class PaperService {
+	
+	private final String scientificPublicatonXSL = "src/main/resources/data/xsl/scientificPaper.xsl";
+	
+	private PaperRepository paperRepository;
+	private XSLTransformer xslTransformer;
+	
+	public PaperService(XSLTransformer xslTransformer, PaperRepository sciPaperRepository) {
+		this.paperRepository = sciPaperRepository;
+		this.xslTransformer = xslTransformer;
+	}
+	
+	public String convertXMLtoHTML(String name) {
+		Document xml = paperRepository.findScientificPaper(name);
+		return xslTransformer.convertXMLtoHTML(scientificPublicatonXSL, xml);
+	}
 }
